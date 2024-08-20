c61ace8222814696ac383e8f11004a5414bc89a1
Yehonatan Rosenblatt <yeohnatan@gmail.com>
2024-08-20 21:57:24 +0300

    zshrc: Upgrade to a better syntax highlighter

--- .config/zsh/plugins.zsh
+++ .config/zsh/plugins.zsh
@@ -11,7 +11,7 @@ source "${ZINIT_HOME}/zinit.zsh"
 zinit ice depth=1; zinit light romkatv/powerlevel10k
 
 # Add plugins
-zinit light zsh-users/zsh-syntax-highlighting
+zinit light zdharma-continuum/fast-syntax-highlighting
 zinit light zsh-users/zsh-autosuggestions
 zinit light zsh-users/zsh-completions
 zinit light Aloxaf/fzf-tab
