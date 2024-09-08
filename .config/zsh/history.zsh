9aa9f8bfeb907bd2e37601aeef4576a78d4e1ec2
Yehonatan Rosenblatt <yeohnatan@gmail.com>
2024-09-08 10:41:38 +0300

    zsh: Remove shared history

--- .config/zsh/history.zsh
+++ .config/zsh/history.zsh
@@ -7,7 +7,6 @@ HISTDUP=erase # Actively erase duplicates from the history file
 # History options
 setopt extended_history       # Write the history file in the ":start:elapsed;command" format.
 setopt inc_append_history     # Immediately append to history after each command
-setopt share_history          # Allow history to be shared across sessions
 setopt hist_expire_dups_first # Overwrite duplicate entries before unique ones if HISTSIZE limit is reached
 setopt hist_ignore_dups       # Don't store an entry if it's a duplicate of the previous one
 setopt hist_ignore_all_dups   # Delete old entry if a new entry is a duplicate
