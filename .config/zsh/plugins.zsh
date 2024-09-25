fe784f19f99489b922191521156922742b22fb57
Yehonatan Rosenblatt <yeohnatan@gmail.com>
2024-09-25 13:19:50 +0300

    tmux: Add $DISPLAY updater

--- .config/zsh/plugins.zsh
+++ .config/zsh/plugins.zsh
@@ -3,6 +3,7 @@ zinit light Aloxaf/fzf-tab
 zinit light zsh-users/zsh-autosuggestions
 zinit light zsh-users/zsh-completions
 zinit ice depth=1; zinit light romkatv/powerlevel10k
+zinit light lljbash/zsh-renew-tmux-env
 
 # Add Oh My ZSH plugins
 zinit snippet OMZP::colored-man-pages
