ee0556a6ead924690df1c509b974756c830f81eb
Yehonatan Rosenblatt <yeohnatan@gmail.com>
2025-02-01 17:27:31 +0200

    zsh: completions: Wrap every text completion

--- .config/zsh/scripts/file_preview.zsh
+++ .config/zsh/scripts/file_preview.zsh
@@ -11,10 +11,10 @@ elif [[ "$category" = "image" ]]; then
     chafa "$path"
     exiftool "$path"
 elif [[ "$category" = "text" ]]; then
-    bat -pp --color=always --line-range=:500 "$path"
+    bat -pp --color=always --line-range=:500 "$path" --wrap character --terminal-width 90
 elif [[ "$kind" = "json" ]]; then
-		jq . "$path" | bat -pp --color=always --line-range=:500 -l json
+		jq . "$path" | bat -pp --color=always --line-range=:500 -l json --wrap character --terminal-width 90
 else
-		file "$path" | fold -w 80 -s
+		file "$path" | fold -w 90 -s
 fi
 
